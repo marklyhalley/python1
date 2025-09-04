164fa77727182efe7b877f5b57a047eb398b15ff
marklyhalley” <hall.secure@proton.me>
2025-09-04 20:49:33 -0300

    Programa Código Cursp

---
+++ Código RM.py
@@ -0,0 +1,4 @@
+Nm = "Marco Tulio Longo Conte"
+RM = 568373
+Dsp = "ESPS"
+print ("Seu nome é",Nm, "\nSeu RM é",RM, "\nO Código do seu curso é:" ,Dsp)
--- aula0.py
+++ aula0.py
@@ -1,5 +1,7 @@
 print ("hello world")
-print ('bye bye')
+print ()
+#print vazio da espaço na tela, \n  faz igualmente
+print ('bye \n bye')
 #comentario simples
 print ("hello people")
 """ COMENTARIO 
@@ -7,3 +9,14 @@ COM MAIS
 DE UMA LINHA
 """
 # idealmente não pode ter dois comandos na mesma linha , caso necessário, utilize ; para separar
+print (10)
+#para printar numero, não necessita de aspas, apenas string
+print ("Minha idade é:", 19)
+#virgula separa elementos no print
+idade = 21
+print (idade)
+msg = "Minha idade é:"
+print (msg, idade)
+x= 2
+y= 4
+print ('O Valor de x é' , x ,"o valor de y é",y)
