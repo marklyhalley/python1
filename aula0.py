d65195cefea2b84ff6195d17ef2d9b2cfd01aa1b
marklyhalley” <hall.secure@proton.me>
2025-09-04 20:24:26 -0300

    hello world

--- aula0.py
+++ aula0.py
@@ -1,2 +1,9 @@
 print ("hello world")
 print ('bye bye')
+#comentario simples
+print ("hello people")
+""" COMENTARIO 
+COM MAIS 
+DE UMA LINHA
+"""
+# idealmente não pode ter dois comandos na mesma linha , caso necessário, utilize ;
