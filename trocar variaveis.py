a92037e7f1a0b8a530fafd4510ad953839a6d8c6
marklyhalley” <hall.secure@proton.me>
2025-09-11 19:44:45 -0300

    Programa agora com texto

--- trocar variaveis.py
+++ trocar variaveis.py
@@ -1,11 +1,10 @@
 
 a = (30)
 b=(20)
-print(a)
-print(b)
+print("o valor de A inicialmente é", a)
+print("o valor de B inicialmente é", b)
 c=a
 a=b
-print(a)
+print("o valor de A no final se torna", a)
 b=c
-print(b)
-print(a+b)
+print("o valor de B no final se torna", b)
